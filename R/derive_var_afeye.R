ec0d52cea35fbbd26a8458955e306c4c862da361
palmenl <lucy.palmen@roche.com>
2022-09-13 11:51:09 +0200

    Update function with LOC/LAT variables

--- R/derive_var_afeye.R
+++ R/derive_var_afeye.R
@@ -12,13 +12,14 @@
 #' @author Lucy Palmen
 #'
 #' @return The input ADSL dataset with an additional column named `STUDYEYE`.
-#'         The occurence dataset with laterality variable (xxLAT)
+#'         The occurence dataset with laterality variable (xxLAT) and location
+#'         variable (xxLOC).
 #' @keywords adsl derivation ophthalmology
 #' @export
 #'
 #' @examples
 #' adsl <- tibble::tribble(
-#'   ~STUDYID, ~USUBJID, ~STUDYEYE
+#'   ~STUDYID, ~USUBJID, ~STUDYEYE,
 #'   "XXX001", "P01", "RIGHT",
 #'   "XXX001", "P02", "LEFT",
 #'   "XXX001", "P03", "LEFT",
@@ -36,20 +37,20 @@
 #'   "XXX001", "P05", "EYE", "RIGHT"
 #' )
 #'
-#' derive_var_afeye(adsl, adae)
+#' derive_var_afeye(adae, adsl, vars(AELOC), vars(AELAT))
 #'
 
 derive_var_afeye <- function(dataset_occ, dataset_adsl,  loc_var, lat_var) {
 
 
-   a<- dataset_occ %>% derive_vars_merged(
+   a<- dataset_occ %>% admiral::derive_vars_merged(
     dataset_add = dataset_adsl,
     by_vars = vars(STUDYID,USUBJID) ) %>%
     mutate(AFEYE= case_when(
       !!sym(vars2chr(loc_var)) =="" ~ "",
-      STUDYEYE=="BILATERAL" ~ "Study Eye",
-      !!sym(vars2chr(lat_var))  == STUDYEYE ~ "Study Eye",
-      !!sym(vars2chr(lat_var)) != STUDYEYE ~ "Fellow Eye",
+      toupper(STUDYEYE)=="BILATERAL" ~ "Study Eye",
+      toupper(!!sym(vars2chr(lat_var)))  == toupper(STUDYEYE) ~ "Study Eye",
+      toupper(!!sym(vars2chr(lat_var))) != toupper(STUDYEYE) ~ "Fellow Eye",
       TRUE ~ ""
     ))
 
