56775bb43910fd1a09bfd85b5fde28e128e5e4c2
berishe3 <eriola.berisha@roche.com>
2022-09-26 15:27:07 +0200

    Resolves #11

---
+++ R/create_sc_test_data.R
@@ -0,0 +1,78 @@
+# Name: create_sc_test_data
+#
+# Label: Create test data for SC domain for study eye info
+#
+# Input: admiral_dm, admiral_sv
+
+library(admiral)
+library(admiral.test) # Contains example datasets from the CDISC pilot project
+library(dplyr)
+library(lubridate)
+library(stringr)
+library(labelled)
+# Load input data
+data("admiral_dm")
+data("admiral_sv")
+
+dm <- admiral_dm
+sv <- admiral_sv
+
+# Remove screen failures, they will not make it to drug infusion
+dm1 <- dm %>%
+  filter(ARMCD != "Scrnfail")
+
+
+# use subjects in DM  and info from SV  Screening 1 visit
+sc <- merge(dm1[, c("STUDYID", "USUBJID", "SUBJID", "RFSTDTC")],
+  sv[sv$VISIT == "SCREENING 1", c("STUDYID", "USUBJID", "SVSTDTC", "VISIT")],
+  by = c("STUDYID", "USUBJID")
+)
+
+# Create SC domain var
+sc$DOMAIN <- "SC"
+sc$SCCAT <- "STUDY EYE SELECTION"
+sc$SCTESTCD <- "FOCID"
+sc$SCTEST <- "Focus of Study-Specific Interest"
+sc$EPOCH <- "SCREENING"
+sc$SCDTC <- sc$SVSTDTC
+sc$SCDY <- as.numeric(as.Date(sc$SCDTC) - as.Date(sc$RFSTDTC))
+# Even SUBJID numbers will have study eye assigned to Left;  odd to Right
+sc$SCORRES <- if_else(as.integer(sc$SUBJID) %% 2 == 0, "Left Eye", "Right Eye")
+sc$SCSTRESC <- if_else(as.integer(sc$SUBJID) %% 2 == 0, "OS", "OD")
+
+
+# SCSEQ;
+sc <- sc %>%
+  group_by(STUDYID, USUBJID) %>%
+  dplyr::mutate(SCSEQ = row_number())
+
+
+# keep relevant variables
+sc <- subset(sc, select = c(
+  "STUDYID", "DOMAIN", "USUBJID", "SCSEQ", "SCTESTCD", "SCTEST",
+  "SCCAT", "SCORRES", "SCSTRESC", "EPOCH", "SCDTC", "SCDY"
+))
+sc <- sc %>%
+  ungroup() %>%
+  # sort data
+  arrange(STUDYID, USUBJID, SCSEQ) %>%
+
+  # add labels
+  set_variable_labels(
+    STUDYID = "Study Identifier",
+    DOMAIN = "Domain Abbreviation",
+    USUBJID = "Unique Subject Identifier",
+    SCSEQ = "Sequence Number",
+    SCTESTCD = "Subject Characteristic Short Name",
+    SCTEST = "Subject Characteristic",
+    SCCAT = "Category for Subject Characteristic",
+    SCORRES = "Result or Finding in Original Units",
+    SCSTRESC = "Character Result/Finding in Std Format",
+    EPOCH = "Epoch",
+    SCDTC = "Date/Time of Collection",
+    SCDY = "Study Day of Examination"
+  )
+
+# ---- Save output ----
+admiral_sc <- sc
+save(admiral_sc, file = "./data/admiral_sc.rda", compress = "bzip2")
