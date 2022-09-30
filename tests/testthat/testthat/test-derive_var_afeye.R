6d6cee5902f15d0c3cfd3f08295ff5103fa62249
palmenl <lucy.palmen@roche.com>
2022-09-30 17:21:17 +0200

    #10 update style

--- tests/testthat/testthat/test-derive_var_afeye.R
+++ tests/testthat/testthat/test-derive_var_afeye.R
@@ -1,5 +1,4 @@
 test_that("AFEYE is derived correctly", {
-
   adae <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~AELOC, ~AELAT, ~STUDYEYE,
     "XXX001", "P01", "EYE", "LEFT", "RIGHT",
@@ -7,7 +6,7 @@ test_that("AFEYE is derived correctly", {
     "XXX001", "P04", "EYE", "RIGHT", "BILATERAL",
     "XXX001", "P05", "EYE", "RIGHT", "RIGHT",
     "XXX002", "P01", "EYE", "LEFT", "RIGHT",
-          )
+  )
   expected_output <- tibble::tribble(
     ~STUDYID, ~USUBJID, ~AELOC, ~AELAT, ~STUDYEYE, ~AFEYE,
     "XXX001", "P01", "EYE", "LEFT", "RIGHT", "Fellow Eye",
