595c24563685a809ce30208b8367a3c0508ac7ac
Mancini, Edoardo {MDBB~Welwyn} <edoardo.mancini@roche.com>
2022-10-21 16:17:15 +0200

    start to make ex test data

---
+++ R/create_ex_testdata.R
@@ -0,0 +1,3 @@
+# Even SUBJID numbers will have study eye assigned to Left;  odd to Right
+sc$SCORRES <- if_else(as.integer(sc$SUBJID) %% 2 == 0, "Left Eye", "Right Eye")
+sc$SCSTRESC <- if_else(as.integer(sc$SUBJID) %% 2 == 0, "OS", "OD")
