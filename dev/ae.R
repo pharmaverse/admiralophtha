36e1a5bb981047def53a4fd2aeb05d6825bd4d7b
Mancini, Edoardo {MDBB~Welwyn} <edoardo.mancini@roche.com>
2022-11-02 15:45:49 +0100

    style ae.r

--- dev/ae.R
+++ dev/ae.R
@@ -13,18 +13,18 @@ library(dplyr)
 
 
 # create possible AELAT values - as collected on CRF ----
-lat = c("LEFT", "RIGHT","BOTH")
+lat <- c("LEFT", "RIGHT", "BOTH")
 
 # read ADMIRAL AE test data ----
-admiral_ophtha_ae = admiral_ae
+admiral_ophtha_ae <- admiral_ae
 
 # create AELAT variable ----
 # with random assignment of lat values where AESOC is "EYE DISORDERS"
 
-admiral_ophtha_ae$AELAT = ifelse(admiral_ophtha_ae$AESOC == "EYE DISORDERS",
-                                 apply(admiral_ophtha_ae, 1, function(x) sample(lat, 1)),'')
+admiral_ophtha_ae$AELAT <- ifelse(admiral_ophtha_ae$AESOC == "EYE DISORDERS",
+  apply(admiral_ophtha_ae, 1, function(x) sample(lat, 1)), ""
+)
 
 # Save dataset ----
-admiral_ae = admiral_ophtha_ae
+admiral_ae <- admiral_ophtha_ae
 saveRDS(admiral_ae, "data/admiral_ae.RDS")
-
