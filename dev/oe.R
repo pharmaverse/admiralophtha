33f8e319d7ee9b1ef6e91f2ec2ddae49de312722
Holmes, William {MDBD~Welwyn} <william.holmes@roche.com>
2022-11-03 18:41:12 +0100

    Run styler::style_file

--- dev/oe.R
+++ dev/oe.R
@@ -22,14 +22,14 @@ sv <- convert_blanks_to_na(admiral_sv)
 dm1 <- dm %>%
   metatools::combine_supp(suppdm)
 
-#dm2 <- select(dm1, c(STUDYID, USUBJID, SAFETY, EFFICACY))
+# dm2 <- select(dm1, c(STUDYID, USUBJID, SAFETY, EFFICACY))
 
 oe1 <- select(sv, c(STUDYID, USUBJID, VISIT, VISITNUM, VISITDY, SVSTDTC)) %>%
   filter(VISITNUM %in% c(1, 3, 5, 7, 8, 8.1, 9, 10, 11, 12)) %>%
   rename(OEDTC = SVSTDTC)
 
 oe2 <- merge(dm1, oe1, by = c("STUDYID", "USUBJID"))
-oestat <- sample(1:100, nrow(oe2)*2, replace = TRUE)
+oestat <- sample(1:100, nrow(oe2) * 2, replace = TRUE)
 
 # BCVA - Visual Acuity Score
 oe31 <- bind_rows(
@@ -44,7 +44,8 @@ oe31 <- bind_rows(
   "OESTRESC" = as.character(OESTRESN),
   "OEORRES" = OESTRESC,
   "OELOC" = "EYE",
-  "OEMETHOD" = "ETDRS EYE CHART")
+  "OEMETHOD" = "ETDRS EYE CHART"
+)
 
 # DRSS
 oe32 <- bind_rows(
@@ -57,11 +58,12 @@ oe32 <- bind_rows(
   "OECAT" = "OPHTHALMIC ASSESSMENTS",
   "OESCAT" = "FORM FOR CFP EVALUATION FOR 7M/4W",
   "oestat" = oestat,
-  "OESTRESN" = ifelse(oestat<2,NA,sample.int(12, n(), replace = TRUE)),
-  "OESTRESC" = ifelse(is.na(OESTRESN),"NOT APPLICABLE",as.character(OESTRESN)),
+  "OESTRESN" = ifelse(oestat < 2, NA, sample.int(12, n(), replace = TRUE)),
+  "OESTRESC" = ifelse(is.na(OESTRESN), "NOT APPLICABLE", as.character(OESTRESN)),
   "OEORRES" = OESTRESC,
   "OELOC" = "RETINA",
-  "OEMETHOD" = "COLOR FUNDUS PHOTOGRAPH")
+  "OEMETHOD" = "COLOR FUNDUS PHOTOGRAPH"
+)
 
 # IOP
 oe33 <- bind_rows(
@@ -72,14 +74,15 @@ oe33 <- bind_rows(
   "OETEST" = "Intraocular Pressure",
   "OECAT" = "INTRAOCULAR PRESSURE",
   "oestat" = oestat,
-  "OESTRESN" = ifelse(oestat>99,NA,round(runif(n(),min = 5,max = 30))),
-  "OESTRESC" = ifelse(is.na(OESTRESN),"",as.character(OESTRESN)),
+  "OESTRESN" = ifelse(oestat > 99, NA, round(runif(n(), min = 5, max = 30))),
+  "OESTRESC" = ifelse(is.na(OESTRESN), "", as.character(OESTRESN)),
   "OEORRES" = OESTRESC,
-  "OEORRESU" = ifelse(is.na(OESTRESN),"","mmHg"),
+  "OEORRESU" = ifelse(is.na(OESTRESN), "", "mmHg"),
   "OESTRESU" = OEORRESU,
-  "OESTAT" = ifelse(is.na(OESTRESN),"NOT DONE",""),
+  "OESTAT" = ifelse(is.na(OESTRESN), "NOT DONE", ""),
   "OELOC" = "EYE",
-  "OEMETHOD" = "APPLANATION TONOMETRY")
+  "OEMETHOD" = "APPLANATION TONOMETRY"
+)
 
 # CST - Central Subfield Thickness
 oe34 <- bind_rows(
@@ -98,41 +101,45 @@ oe34 <- bind_rows(
   "OEORRESU" = "um",
   "OESTRESU" = "um",
   "OELOC" = "RETINA",
-  "OEMETHOD" = "SD-OCT")
+  "OEMETHOD" = "SD-OCT"
+)
 
-oe4 <- bind_rows(oe31,oe32,oe33,oe34) %>%
+oe4 <- bind_rows(oe31, oe32, oe33, oe34) %>%
   arrange(STUDYID, USUBJID, VISITNUM, OEDTC, OETESTCD, OELAT) %>%
   group_by(STUDYID, USUBJID) %>%
   mutate(
     "OESEQ" = row_number(),
     "DOMAIN" = "OE",
-    "OEDY" = as.numeric(as.Date(OEDTC)-as.Date(RFSTDTC)) + (as.Date(OEDTC)>=as.Date(RFSTDTC)),
+    "OEDY" = as.numeric(as.Date(OEDTC) - as.Date(RFSTDTC)) + (as.Date(OEDTC) >= as.Date(RFSTDTC)),
     "OETPT" = "",
-    "OETPTNUM" = NA)
+    "OETPTNUM" = NA
+  )
 
 oe <- oe4 %>%
-  select(STUDYID,DOMAIN,USUBJID,OESEQ,OECAT,OESCAT,OEDTC,VISIT,VISITNUM,VISITDY,
-         OESTRESN,OESTRESC,OEORRES,OETEST,OETESTCD,OETSTDTL,OELAT,OELOC,OEDY,
-         OEMETHOD,OEORRESU,OESTRESU,OESTAT,OETPT,OETPTNUM) %>%
+  select(
+    STUDYID, DOMAIN, USUBJID, OESEQ, OECAT, OESCAT, OEDTC, VISIT, VISITNUM, VISITDY,
+    OESTRESN, OESTRESC, OEORRES, OETEST, OETESTCD, OETSTDTL, OELAT, OELOC, OEDY,
+    OEMETHOD, OEORRESU, OESTRESU, OESTAT, OETPT, OETPTNUM
+  ) %>%
   set_variable_labels(
     STUDYID = "Study Identifier",
-    DOMAIN	= "Domain Abbreviation",
+    DOMAIN = "Domain Abbreviation",
     USUBJID = "Unique Subject Identifier",
-    OESEQ	= "Sequence Number",
-    OECAT	= "Category for Ophthalmic Test or Exam",
+    OESEQ = "Sequence Number",
+    OECAT = "Category for Ophthalmic Test or Exam",
     OESCAT = "Subcategory for Ophthalmic Test or Exam",
-    OEDTC	= "Date/Time of Collection",
-    OESTRESN	= "Numeric Result/Finding in Standard Units",
-    OESTRESC	= "Character Result/Finding in Std Format",
-    OEORRES	= "Result or Finding in Original Units",
-    OETEST	= "Name of Ophthalmic Test or Examination",
-    OETESTCD	= "Short Name of Ophthalmic Test or Exam",
+    OEDTC = "Date/Time of Collection",
+    OESTRESN = "Numeric Result/Finding in Standard Units",
+    OESTRESC = "Character Result/Finding in Std Format",
+    OEORRES = "Result or Finding in Original Units",
+    OETEST = "Name of Ophthalmic Test or Examination",
+    OETESTCD = "Short Name of Ophthalmic Test or Exam",
     OETSTDTL = "Ophthalmic Test or Exam Detail",
-    OELAT	= "Laterality",
-    OELOC	= "Location Used for the Measurement",
-    OEDY	= "Study Day of Visit/Collection/Exam",
-    VISIT	= "Visit Name",
-    VISITNUM	= "Visit Number",
+    OELAT = "Laterality",
+    OELOC = "Location Used for the Measurement",
+    OEDY = "Study Day of Visit/Collection/Exam",
+    VISIT = "Visit Name",
+    VISITNUM = "Visit Number",
     VISITDY = "Planned Study Day of Visit",
     OEMETHOD = "Method of Test or Examination",
     OEORRESU = "Original Units",
