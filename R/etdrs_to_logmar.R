49508e8c2110dac7b1047f6b6a100b27411ab9ad
Linacre, Rachel {MDBA~Welwyn} <rachel.linacre@roche.com>
2023-01-23 19:03:43 +0100

    create etdrs to logmar function

---
+++ R/etdrs_to_logmar.R
@@ -0,0 +1,36 @@
+#' ETDRS --> LogMAR conversion
+#'
+#' Convert ETDRS --> LogMAR conversion in (`AVAL`)
+#'
+#' @param variable variable containing ETDRS score to convert to logMAR
+#'
+#' @details
+#' ETDRS is converted by logMAR = -0.02 * ETDRS + 1.7
+#'
+#' @author Rachel Linacre
+#'
+#' @return The input variable converted
+#' @keywords
+#' @export
+#'
+#' @examples
+#' library(tibble)
+#' library(dplyr)
+#' library(admiral)
+#'
+#' adbcva <- tribble(
+#'   ~STUDYID, ~USUBJID, ~AVAL,
+#'   "XXX001", "P01", 5,
+#'   "XXX001", "P02", 10,
+#'   "XXX001", "P03", 15,
+#'   "XXX001", "P04", 20,
+#'   "XXX001", "P05", 25
+#' )
+#'
+#' adbcva <- adbcva %>% mutate(AVAL = etdrs_to_logmar(AVAL))
+
+etdrs_to_logmar <- function(variable) {
+
+  variable <- -0.02 * variable + 1.7
+
+}
