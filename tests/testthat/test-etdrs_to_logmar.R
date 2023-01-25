d84d9eae714ef1bf212e45b5dd93558d0b05a9e6
Linacre, Rachel {MDBA~Welwyn} <rachel.linacre@roche.com>
2023-01-25 13:41:08 +0100

    test

---
+++ tests/testthat/test-etdrs_to_logmar.R
@@ -0,0 +1,13 @@
+test_that("ETDRS converted correctly", {
+
+  input <- data.frame (AVAL  = c(5,10,15,20,25,30,90))
+
+  expected_output <-  data.frame(AVAL = c(1.6,1.5,1.4,1.3,1.2,1.1,-0.1))
+
+  admiraldev::expect_dfs_equal(
+etdrs_to_logmar(input),
+    expected_output,
+    keys = c("AVAL")
+  )
+
+})
