eb32161d8f98f18477b90a147eb1b358d84f9f99
Linacre, Rachel {MDBA~Welwyn} <rachel.linacre@roche.com>
2023-02-03 17:03:12 +0100

    update to add check in function that input is numeric

--- tests/testthat/test-calculate_etdrs_to_logmar.R
+++ tests/testthat/test-calculate_etdrs_to_logmar.R
@@ -1,11 +1,11 @@
 test_that("ETDRS converted correctly", {
-  input <- data.frame(AVAL = c(5, 10, 15, 20, 25, 30, 90))
+  input <- c(5, 10, 15, 20, 25, 30, 90)
 
-  expected_output <- data.frame(AVAL = c(1.6, 1.5, 1.4, 1.3, 1.2, 1.1, -0.1))
+  expected_output <- c(1.6, 1.5, 1.4, 1.3, 1.2, 1.1, -0.1)
   actual_output <- calculate_etdrs_to_logmar(input)
 
   expect_equal(
-    actual_output$AVAL,
-    expected_output$AVAL,
+    actual_output,
+    expected_output,
   )
 })
