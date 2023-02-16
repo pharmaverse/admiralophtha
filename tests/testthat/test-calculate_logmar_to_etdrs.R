04f408b70cdf180d3a56d1d6067aff88d970a2c6
Linacre, Rachel {MDBA~Welwyn} <rachel.linacre@roche.com>
2023-02-16 18:00:17 +0100

    try to fix tests

--- tests/testthat/test-calculate_logmar_to_etdrs.R
+++ tests/testthat/test-calculate_logmar_to_etdrs.R
@@ -1,4 +1,4 @@
-test_that("Logmar converted correctly", {
+test_that("LogMAR converted correctly", {
   input <- c(1.6, 1.5, 1.4, 1.3, 1.2, 1.1, -0.1)
 
   expected_output <- c(5, 10, 15, 20, 25, 30, 90)
