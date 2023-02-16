9e30868535418888ecc4f4aef0518dbce4f90652
Linacre, Rachel {MDBA~Welwyn} <rachel.linacre@roche.com>
2023-02-16 16:31:47 +0100

    updates

--- R/calculate_logmar_to_etdrs.R
+++ R/calculate_logmar_to_etdrs.R
@@ -27,11 +27,10 @@
 #'   "XXX001", "P05", "VACSCORE", "ETDRS EYE CHART", 1
 #' )
 #'
-#' adbcva <- oe %>% filter(OETESTCD == "VACSCORE" & toupper(OEMETHOD) == "LOGMAR EYE CHART") %>%
-#'                  mutate(OESTRESN = calculate_logmar_to_etdrs(OESTRESN))
-
+#' adbcva <- oe %>%
+#'   filter(OETESTCD == "VACSCORE" & toupper(OEMETHOD) == "LOGMAR EYE CHART") %>%
+#'   mutate(OESTRESN = calculate_logmar_to_etdrs(OESTRESN))
 calculate_logmar_to_etdrs <- function(value) {
   assert_numeric_vector(value)
-  - (value - 1.7) / 0.02
-
+  -(value - 1.7) / 0.02
 }
