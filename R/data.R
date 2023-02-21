a2c6e8964ee6aea438e4a0a8b1b7d15b3a96ff56
Mancini, Edoardo {MDBB~Welwyn} <edoardo.mancini@roche.com>
2023-02-21 12:18:51 +0100

    Update

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Best Corrected Visual Acuity Analysis Dataset
+#'
+#' An example Best Corrected Visual Acuity (BCVA) analysis dataset
+#' @keywords datasets
+#' @family datasets
+#' @source
+#' Derived from the `dm` and `ds` datasets using `{admiral`, `{admiralophtha}` and
+#' (\url{https://github.com/pharmaverse/admiralophtha/blob/main/inst/templates/ad_adbcva.R})
+#'
+"admiral_adbcva"
