d41bf3d3ca7784fc9122f0ac0be434bb6c960ccc
Gao, Jane {MDDG~Beijing} <jane.gao@roche.com>
2023-06-02 13:07:37 +0200

    keep up to date with devel branch

--- R/derive_var_afeye.R
+++ R/derive_var_afeye.R
@@ -24,24 +24,33 @@
 #'   ~STUDYID, ~USUBJID, ~STUDYEYE, ~AELOC, ~AELAT,
 #'   "XXX001", "P01", "RIGHT", "EYE", "RIGHT",
 #'   "XXX001", "P01", "RIGHT", "EYE", "LEFT",
+#'   "XXX001", "P01", "RIGHT", "EYE", "",
+#'   "XXX001", "P01", "RIGHT", "", "RIGHT",
 #'   "XXX001", "P02", "LEFT", "", "",
 #'   "XXX001", "P02", "LEFT", "EYE", "LEFT",
 #'   "XXX001", "P04", "BILATERAL", "EYE", "RIGHT",
-#'   "XXX001", "P05", "RIGHT", "EYE", "RIGHT"
+#'   "XXX001", "P05", "RIGHT", "EYE", "RIGHT",
+#'   "XXX001", "P05", "RIGHT", "EYE", "BILATERAL",
+#'   "XXX001", "P06", "BILATERAL", "", "",
+#'   "XXX001", "P06", "BILATERAL", "", "RIGHT",
+#'   "XXX001", "P07", "BILATERAL", "EYE", "BILATERAL",
+#'   "XXX001", "P08", "", "EYE", "BILATERAL",
 #' )
 #'
-#' derive_var_afeye(adae, AELOC, AELAT)
+#' adae <- derive_var_afeye(adae, AELOC, AELAT)
 derive_var_afeye <- function(dataset_occ, loc_var, lat_var) {
   loc_var <- assert_symbol(enexpr(loc_var))
   lat_var <- assert_symbol(enexpr(lat_var))
-  assert_data_frame(dataset_occ, required_vars = expr_c(loc_var, lat_var))
+  assert_data_frame(dataset_occ, required_vars = expr_c(loc_var, lat_var, exprs(STUDYEYE)))
 
   dataset_occ %>%
     mutate(AFEYE = case_when(
-      !!loc_var == "" ~ "",
-      toupper(STUDYEYE) == "BILATERAL" ~ "Study Eye",
-      toupper(!!lat_var) == toupper(STUDYEYE) ~ "Study Eye",
-      toupper(!!lat_var) != toupper(STUDYEYE) ~ "Fellow Eye",
-      TRUE ~ ""
+      STUDYEYE != "" & !!lat_var == "BILATERAL" & !!loc_var == "EYE" ~ "Both Eyes",
+      toupper(STUDYEYE) == "BILATERAL" & !!lat_var != "" & !!loc_var == "EYE" ~ "Study Eye",
+      toupper(!!lat_var) == toupper(STUDYEYE) & STUDYEYE != "" & !!lat_var != "" &
+        !!loc_var == "EYE" ~ "Study Eye",
+      toupper(!!lat_var) != toupper(STUDYEYE) & STUDYEYE != "" & !!lat_var != "" &
+        !!loc_var == "EYE" ~ "Fellow Eye",
+      TRUE ~ NA_character_
     ))
 }
