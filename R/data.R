616d5aa08cb3a939555c988f40327a2696c32849
Gao, Jane {MDDG~Beijing} <jane.gao@roche.com>
2023-06-02 11:05:06 +0200

    add admiralophtha_qs.Rd

--- R/data.R
+++ R/data.R
@@ -40,3 +40,14 @@
 #' (\url{https://github.com/pharmaverse/admiralophtha/blob/main/inst/templates/ad_adoe.R})
 #'
 "admiralophtha_adoe"
+
+#' Ophthalmology Questionnaires SDTM Dataset
+#'
+#' An example Questionnaires SDTM dataset with ophthalmology-specific questionnaire of NEI VFQ-25
+#' @keywords datasets
+#' @family datasets
+#' @source
+#' Constructed using `qs` from the `{admiral` package
+#'
+"admiralophtha_qs"
+
