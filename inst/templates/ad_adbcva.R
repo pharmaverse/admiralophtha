7b7297a2b51d00110e9e14d47e21abdd804ae4ce
steventing12 <steven.ting@cytel.com>
2024-11-07 06:13:10 +0000

    Closes #244 Used derive_vars_cat() to derive AVALCA1N and AVALCAT1, replacing look-up table with derive_vars_merged()

--- inst/templates/ad_adbcva.R
+++ inst/templates/ad_adbcva.R
@@ -41,49 +41,49 @@ param_lookup <- tibble::tribble(
 # SBCVA and FBCVA definition list
 # This can be sourced from a separate R script
 definition_bcva <- exprs(
-  ~PARAMCD,  ~condition,              ~AVALCA1N,    ~AVALCAT1,
-  "SBCVA",   AVAL >= 0 & AVAL <= 3,   1000,          "< 20/800",
-  "FBCVA",   AVAL >= 0 & AVAL <= 3,   1000,          "< 20/800",
-  "SBCVA",   AVAL >= 4 & AVAL <= 8,   800,           "20/800",
-  "FBCVA",   AVAL >= 4 & AVAL <= 8,   800,           "20/800",
-  "SBCVA",   AVAL >= 9 & AVAL <= 13,  640,           "20/640",
-  "FBCVA",   AVAL >= 9 & AVAL <= 13,  640,           "20/640",
-  "SBCVA",   AVAL >= 14 & AVAL <= 18, 500,           "20/500",
-  "FBCVA",   AVAL >= 14 & AVAL <= 18, 500,           "20/500",
-  "SBCVA",   AVAL >= 19 & AVAL <= 23, 400,           "20/400",
-  "FBCVA",   AVAL >= 19 & AVAL <= 23, 400,           "20/400",
-  "SBCVA",   AVAL >= 24 & AVAL <= 28, 320,           "20/320",
-  "FBCVA",   AVAL >= 24 & AVAL <= 28, 320,           "20/320",
-  "SBCVA",   AVAL >= 29 & AVAL <= 33, 250,           "20/250",
-  "FBCVA",   AVAL >= 29 & AVAL <= 33, 250,           "20/250",
-  "SBCVA",   AVAL >= 34 & AVAL <= 38, 200,           "20/200",
-  "FBCVA",   AVAL >= 34 & AVAL <= 38, 200,           "20/200",
-  "SBCVA",   AVAL >= 39 & AVAL <= 43, 160,           "20/160",
-  "FBCVA",   AVAL >= 39 & AVAL <= 43, 160,           "20/160",
-  "SBCVA",   AVAL >= 44 & AVAL <= 48, 125,           "20/125",
-  "FBCVA",   AVAL >= 44 & AVAL <= 48, 125,           "20/125",
-  "SBCVA",   AVAL >= 49 & AVAL <= 53, 100,           "20/100",
-  "FBCVA",   AVAL >= 49 & AVAL <= 53, 100,           "20/100",
-  "SBCVA",   AVAL >= 54 & AVAL <= 58, 80,            "20/80",
-  "FBCVA",   AVAL >= 54 & AVAL <= 58, 80,            "20/80",
-  "SBCVA",   AVAL >= 59 & AVAL <= 63, 63,            "20/63",
-  "FBCVA",   AVAL >= 59 & AVAL <= 63, 63,            "20/63",
-  "SBCVA",   AVAL >= 64 & AVAL <= 68, 50,            "20/50",
-  "FBCVA",   AVAL >= 64 & AVAL <= 68, 50,            "20/50",
-  "SBCVA",   AVAL >= 69 & AVAL <= 73, 40,            "20/40",
-  "FBCVA",   AVAL >= 69 & AVAL <= 73, 40,            "20/40",
-  "SBCVA",   AVAL >= 74 & AVAL <= 78, 32,            "20/32",
-  "FBCVA",   AVAL >= 74 & AVAL <= 78, 32,            "20/32",
-  "SBCVA",   AVAL >= 79 & AVAL <= 83, 25,            "20/25",
-  "FBCVA",   AVAL >= 79 & AVAL <= 83, 25,            "20/25",
-  "SBCVA",   AVAL >= 84 & AVAL <= 88, 20,            "20/20",
-  "FBCVA",   AVAL >= 84 & AVAL <= 88, 20,            "20/20",
-  "SBCVA",   AVAL >= 89 & AVAL <= 93, 16,            "20/16",
-  "FBCVA",   AVAL >= 89 & AVAL <= 93, 16,            "20/16",
-  "SBCVA",   AVAL >= 94 & AVAL <= 97, 12,            "20/12",
-  "FBCVA",   AVAL >= 94 & AVAL <= 97, 12,            "20/12",
-  "SBCVA",   AVAL >= 98,               1,             "> 20/12",
-  "FBCVA",   AVAL >= 98,               1,             "> 20/12"
+  ~PARAMCD, ~condition, ~AVALCA1N, ~AVALCAT1,
+  "SBCVA", AVAL >= 0 & AVAL <= 3, 1000, "< 20/800",
+  "FBCVA", AVAL >= 0 & AVAL <= 3, 1000, "< 20/800",
+  "SBCVA", AVAL >= 4 & AVAL <= 8, 800, "20/800",
+  "FBCVA", AVAL >= 4 & AVAL <= 8, 800, "20/800",
+  "SBCVA", AVAL >= 9 & AVAL <= 13, 640, "20/640",
+  "FBCVA", AVAL >= 9 & AVAL <= 13, 640, "20/640",
+  "SBCVA", AVAL >= 14 & AVAL <= 18, 500, "20/500",
+  "FBCVA", AVAL >= 14 & AVAL <= 18, 500, "20/500",
+  "SBCVA", AVAL >= 19 & AVAL <= 23, 400, "20/400",
+  "FBCVA", AVAL >= 19 & AVAL <= 23, 400, "20/400",
+  "SBCVA", AVAL >= 24 & AVAL <= 28, 320, "20/320",
+  "FBCVA", AVAL >= 24 & AVAL <= 28, 320, "20/320",
+  "SBCVA", AVAL >= 29 & AVAL <= 33, 250, "20/250",
+  "FBCVA", AVAL >= 29 & AVAL <= 33, 250, "20/250",
+  "SBCVA", AVAL >= 34 & AVAL <= 38, 200, "20/200",
+  "FBCVA", AVAL >= 34 & AVAL <= 38, 200, "20/200",
+  "SBCVA", AVAL >= 39 & AVAL <= 43, 160, "20/160",
+  "FBCVA", AVAL >= 39 & AVAL <= 43, 160, "20/160",
+  "SBCVA", AVAL >= 44 & AVAL <= 48, 125, "20/125",
+  "FBCVA", AVAL >= 44 & AVAL <= 48, 125, "20/125",
+  "SBCVA", AVAL >= 49 & AVAL <= 53, 100, "20/100",
+  "FBCVA", AVAL >= 49 & AVAL <= 53, 100, "20/100",
+  "SBCVA", AVAL >= 54 & AVAL <= 58, 80, "20/80",
+  "FBCVA", AVAL >= 54 & AVAL <= 58, 80, "20/80",
+  "SBCVA", AVAL >= 59 & AVAL <= 63, 63, "20/63",
+  "FBCVA", AVAL >= 59 & AVAL <= 63, 63, "20/63",
+  "SBCVA", AVAL >= 64 & AVAL <= 68, 50, "20/50",
+  "FBCVA", AVAL >= 64 & AVAL <= 68, 50, "20/50",
+  "SBCVA", AVAL >= 69 & AVAL <= 73, 40, "20/40",
+  "FBCVA", AVAL >= 69 & AVAL <= 73, 40, "20/40",
+  "SBCVA", AVAL >= 74 & AVAL <= 78, 32, "20/32",
+  "FBCVA", AVAL >= 74 & AVAL <= 78, 32, "20/32",
+  "SBCVA", AVAL >= 79 & AVAL <= 83, 25, "20/25",
+  "FBCVA", AVAL >= 79 & AVAL <= 83, 25, "20/25",
+  "SBCVA", AVAL >= 84 & AVAL <= 88, 20, "20/20",
+  "FBCVA", AVAL >= 84 & AVAL <= 88, 20, "20/20",
+  "SBCVA", AVAL >= 89 & AVAL <= 93, 16, "20/16",
+  "FBCVA", AVAL >= 89 & AVAL <= 93, 16, "20/16",
+  "SBCVA", AVAL >= 94 & AVAL <= 97, 12, "20/12",
+  "FBCVA", AVAL >= 94 & AVAL <= 97, 12, "20/12",
+  "SBCVA", AVAL >= 98, 1, "> 20/12",
+  "FBCVA", AVAL >= 98, 1, "> 20/12"
 )
 
 # ---- Derivations ----
@@ -281,11 +281,11 @@ adbcva_crtflag <- adbcva_adsl %>%
     ),
     filter = PARAMCD %in% c("SBCVA", "FBCVA")
   ) %>%
-# Add AVALCATx variables
-derive_vars_cat(
-  definition = definition_bcva,
-  by_vars = exprs(PARAMCD)
-)
+  # Add AVALCATx variables
+  derive_vars_cat(
+    definition = definition_bcva,
+    by_vars = exprs(PARAMCD)
+  )
 
 
 # Final Steps, Select final variables and Add labels
