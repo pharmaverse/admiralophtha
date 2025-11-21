8cbb3cef7931045e8f933b8e69e430e3909b634f
Edoardo Mancini <53403957+manciniedoardo@users.noreply.github.com>
2025-11-21 10:13:37 +0000

    #280 #286 lintr and advfq template update (#287)

--- .lintr.R
+++ .lintr.R
@@ -1,8 +1,12 @@
-library(lintr)
+# source in temporary environment to avoid changing the global environment
+temp_env <- new.env(parent = globalenv())
 
-source(system.file("lintr/linters.R", package = "admiraldev"))
+source(system.file("lintr/linters.R", package = "admiraldev"), local = temp_env)
 
-linters <- admiral_linters()
+linters <- temp_env$admiral_linters()
+
+# remove temporary environment to avoid lintr warning regarding "unused settings"
+rm(temp_env)
 
 exclusions <- list(
   "R/data.R" = Inf,
